747e9af29415f299a87c4cbc7749cad529bf4890
LATIM RONNIE DAVID <89303631+latrondav@users.noreply.github.com>
2023-12-05 10:11:21 +0300

    Update settings.py

--- GeromaProject/settings.py
+++ GeromaProject/settings.py
@@ -134,7 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Kampala'
 
 USE_I18N = True
 
@@ -154,3 +154,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SECURE_SSL_REDIRECT = True
+
+# SESSION_EXPIRE_AT_BROWSER_CLOSE = True
